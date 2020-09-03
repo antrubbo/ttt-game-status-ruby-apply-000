b20b176fee2eb0a89f0643f672cc348a80f96afc
Anthony Rubbo <anthony.rubbo@gmail.com>
2020-09-03 14:54:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -50,7 +50,9 @@ def winner(board)
   win_index1 = win[0]
   win_index2 = win[1]
   win_index3 = win[2]
-  if win
+  if !win
+    nil
+  else
     if board[win_index1] == "X"
       return "X"
     else board[win_index1] == "O"
